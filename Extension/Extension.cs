28f3e6872008d19deb7f480faf17e451f1e48637
Milkis2022 <108617300+Milkis2022@users.noreply.github.com>
2023-05-01 08:10:51 +0900

    Update Extension.cs

--- Extension/Extension.cs
+++ Extension/Extension.cs
@@ -1,1 +1,53 @@
+using System;
+
+namespace Extension_study
+{
+    public class Calculator
+    {
+        public void Add()
+        {
+            Console.WriteLine("Add() function called");
+        }
+        public void Subtract()
+        {
+            Console.WriteLine("Subtract() function called");
+        }
+
+        public void Multiply()
+        {
+            Console.WriteLine("Multiply() function called");
+        }
+    }
+
+    public static class Extension
+    {   
+
+        // static 메서드로 선언하며 매개변수로는 (this ClassName Name) 형식으로 작성
+        // 정적클래스에 선언하며 정적 메서드로 선언하지만 인스턴스 메서드처럼 사용함
+        public static void Divide(this Calculator cal)
+        {
+            Console.WriteLine("Divide() function called");
+        }
+        
+        // string 에 대한 확장 메서드
+
+        public static void Hello(this string str)
+        {
+            Console.WriteLine($"{str} Hello!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator cal = new Calculator();
+            cal.Add();
+            cal.Divide();               // 확장 메서드로 추가한 메서드
+
+            string jerry = "Jerry";
+            jerry.Hello();              // 확장 메서드로 추가한 메서드
+        }
+    }
+
+}
 
